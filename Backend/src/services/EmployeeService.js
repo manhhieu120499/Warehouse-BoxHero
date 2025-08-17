95f2fbf10258cb28e384eb808f64b36277e3380a
manhhieu120499 <vomanhhieu62@gmail.com>
2025-08-17 16:52:02 +0700

    update EmployeeService

--- Backend/src/services/EmployeeService.js
+++ Backend/src/services/EmployeeService.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize');
 const db = require('../../models');
 const dotenv = require('dotenv');
 const Employee = db.Employee;
@@ -9,6 +10,7 @@ dotenv.config();
 const HTTP_OK = process.env.HTTP_OK;
 const HTTP_NOT_FOUND = process.env.HTTP_NOT_FOUND;
 const HTTP_INTERNAL_SERVER_ERROR = process.env.HTTP_INTERNAL_SERVER_ERROR;
+const HTTP_BAD_REQUEST = process.env.HTTP_BAD_REQUEST
 
 class EmployeeService {
     findEmployee(employeeID) {
@@ -20,6 +22,11 @@ class EmployeeService {
                         {
                             model: Account,
                             as: 'account',
+                            include: {
+                                model: Role,
+                                as: 'roles',
+                                through: {attributes: []}
+                            }
                         },
                     ],
                 });
@@ -52,10 +59,13 @@ class EmployeeService {
             }
         });
     }
-    findAllEmployee() {
+    findAllEmployee(adminId) {
         return new Promise(async (resolve, reject) => {
             try {
                 const employees = await Employee.findAll({
+                    where: {
+                        employeeID: {[Op.ne]: adminId}
+                    },
                     include: [
                         {
                             model: Account,
@@ -64,16 +74,24 @@ class EmployeeService {
                                 {
                                     model: Role,
                                     as: 'roles',
+                                    through: {attributes: []}
                                 },
                             ],
                         },
                     ],
                 });
+                const responseEmployee = []
+                employees.forEach(emp => {
+                     const {account, ...response} = emp.toJSON()
+                     const {roles, ...restAccount} = account
+                     responseEmployee.push({...response, statusWork: account.statusWork, roles})
+                })
+
                 resolve({
                     status: 'OK',
                     statusHttp: HTTP_OK,
                     message: 'Lấy danh sách nhân viên thành công',
-                    employees,
+                    employees: responseEmployee,
                 });
             } catch (err) {
                 reject({
@@ -83,6 +101,80 @@ class EmployeeService {
                 });
             }
         });
+    }
+    updateEmployee(employee) {
+        return new Promise(async (resolve, reject) => {
+            const transaction = await sequelize.transaction()
+            try{
+                const {employeeID,
+                        employeeName,
+                        image,
+                        cccd,
+                        dob,
+                        phoneNumber,
+                        gender,
+                        address,
+                        startDate,
+                        endDate,
+                        statusWork,
+                        warehouseID} = employee
+                let updateEmployee = {
+                        employeeID,
+                        employeeName,
+                        image,
+                        cccd,
+                        dob,
+                        phoneNumber,
+                        gender,
+                        address,
+                        startDate,
+                        statusWork,
+                        warehouseID
+                }
+                const checkExist = await Employee.findOne({
+                    where: {employeeID}
+                })
+                if(!checkExist) {
+                    resolve({
+                        status: 'ERR',
+                        statusHttp: HTTP_NOT_FOUND,
+                        message: 'Nhân viên không tồn tại'
+                    })
+                }
+                if(statusWork == 'inactive') {
+                    if(!endDate) {
+                        resolve({
+                        status: 'ERR',
+                        statusHttp: HTTP_BAD_REQUEST,
+                        message: 'Ngày kết thúc bắt buộc khi nhân viên nghỉ việc'
+                    }) }else {
+                        updateEmployee.endDate = endDate
+                        // update account
+                    }
+                }
+                const updateResponse = await Employee.update({
+                        employeeID,
+                        employeeName,
+                        image,
+                        cccd,
+                        dob,
+                        phoneNumber,
+                        gender,
+                        address,
+                        startDate,
+                        warehouseID
+                })
+                await transaction.commit()
+            }catch(err) {
+                await transaction.rollback()
+                reject({
+                    status: 'ERR',
+                    statusHttp: HTTP_INTERNAL_SERVER_ERROR,
+                    message: err,
+                })
+                
+            }
+        })
     }
 }
 
