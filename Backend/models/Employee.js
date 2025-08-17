203ec2dd3b932e38476e59dee7a21bfaff525606
Bao <hbao27121@gmail.com>
2025-08-17 20:26:05 +0700

    add Employee

---
+++ Backend/models/Employee.js
@@ -0,0 +1,65 @@
+module.exports = (sequelize, Sequelize) => {
+    const Employee = sequelize.define(
+        'Employee',
+        {
+            employeeID: {
+                type: Sequelize.STRING,
+                primaryKey: true,
+            },
+            employeeName: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            cccd: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            dob: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            phoneNumber: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            gender: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            image: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            address: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            startDate: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            endDate: {
+                type: Sequelize.DATE,
+                allowNull: true,
+            },
+            status: {
+                type: Sequelize.ENUM('ACTIVE', 'INACTIVE'),
+                allowNull: false,
+            },
+            warehouseID: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+        },
+        {
+            tableName: 'employees',
+            timestamps: false,
+        },
+    );
+
+    Employee.associate = (models) => {
+        Employee.hasOne(models.Account, { foreignKey: 'employeeID', as: 'account' });
+    };
+
+    return Employee;
+};
