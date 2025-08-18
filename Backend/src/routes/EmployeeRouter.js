c1211cd4f7779169245721ca3f2130003cb10f20
manhhieu120499 <vomanhhieu62@gmail.com>
2025-08-18 18:20:05 +0700

    commit api employee

--- Backend/src/routes/EmployeeRouter.js
+++ Backend/src/routes/EmployeeRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { authUser, authUserIsManager } = require('../middleware/AuthMiddleware');
 const validate = require('../validates/validate');
 const EmployeeController = require('../controllers/EmployeeController');
-const { checkEmail } = require('../validates/employee.validation');
+const { checkEmail, checkUpdateEmployee } = require('../validates/employee.validation');
 const router = express.Router();
 router.get('/', (req, res) => {
     return res.send('EmployeeRouter Router is working!');
@@ -10,4 +10,6 @@ router.get('/', (req, res) => {
 
 router.post('/list', authUserIsManager, EmployeeController.getAllEmployee);
 router.post('/employee-detail', authUser, checkEmail, validate, EmployeeController.getEmployee);
+router.put('/update', authUserIsManager, checkUpdateEmployee, validate, EmployeeController.updateEmployee)
+router.get('/filter', authUserIsManager, EmployeeController.searchEmployee)
 module.exports = router;
--- Backend/src/validates/employee.validation.js
+++ Backend/src/validates/employee.validation.js
@@ -1,9 +1,113 @@
 const { query, body } = require('express-validator');
+const ALLOWED_ROLES = ['SYSTEM_ADMIN', 'WARE_MANAGER', 'STOCK_RECEIVER', 'STOCK_DISPATCHER', 'ACCOUNTANT'];
 
 const checkEmail = [
     body('email').notEmpty().withMessage('Email là bắt buộc').bail().isEmail().withMessage('Email không hợp lệ'),
 ];
 
+const checkUpdateEmployee = [
+    body('employeeID').notEmpty().withMessage("Mã nhân viên là bắt buộc").bail(),
+    body('status')
+            .optional({ nullable: true, checkFalsy: true })
+            .isIn(['ACTIVE', 'INACTIVE'])
+            .withMessage('Trạng thái công việc phải là "ACTIVE" hoặc "INACTIVE"')
+            .bail()
+            .default('ACTIVE'),
+        body('employeeName')
+            .notEmpty()
+            .withMessage('Tên nhân viên là bắt buộc')
+            .bail()
+            .isString()
+            .withMessage('Tên nhân viên phải là một chuỗi')
+            .bail()
+            .isLength({ min: 2, max: 100 })
+            .withMessage('Tên nhân viên phải có độ dài từ 2 đến 100 ký tự'),
+        body('cccd')
+            .notEmpty()
+            .withMessage('CCCD là bắt buộc')
+            .bail()
+            .isString()
+            .withMessage('CCCD phải là một chuỗi')
+            .bail()
+            .isLength(12)
+            .withMessage('CCCD phải có độ dài 12 ký tự'),
+        body('dob')
+            .notEmpty()
+            .withMessage('Ngày sinh là bắt buộc')
+            .bail()
+            .isDate()
+            .withMessage('Ngày sinh không hợp lệ')
+            .bail()
+            .custom((value) => {
+                const today = new Date();
+                const dob = new Date(value);
+                if (dob >= today) {
+                    throw new Error('Ngày sinh phải là trong quá khứ');
+                }
+                return true;
+            }),
+        body('phoneNumber')
+            .notEmpty()
+            .withMessage('Số điện thoại là bắt buộc')
+            .bail()
+            .isString()
+            .withMessage('Số điện thoại phải là một chuỗi')
+            .bail()
+            .isLength({ min: 10, max: 15 })
+            .withMessage('Số điện thoại phải có độ dài từ 10 đến 15 ký tự')
+            .bail()
+            .custom((value) => {
+                const regex = /^[0-9]+$/;
+                if (!regex.test(value)) {
+                    throw new Error('Số điện thoại không hợp lệ');
+                }
+                return true;
+            }),
+        body('gender')
+            .notEmpty()
+            .withMessage('Giới tính là bắt buộc')
+            .bail()
+            .isIn(['male', 'female', 'other'])
+            .withMessage('Giới tính không hợp lệ'),
+        body('image')
+            .notEmpty()
+            .withMessage('Hình ảnh là bắt buộc')
+            .bail()
+            .isURL()
+            .withMessage('Hình ảnh phải là một URL hợp lệ'),
+        body('address')
+            .notEmpty()
+            .withMessage('Địa chỉ là bắt buộc')
+            .bail()
+            .isString()
+            .withMessage('Địa chỉ phải là một chuỗi')
+            .bail()
+            .isLength({ min: 5, max: 200 })
+            .withMessage('Địa chỉ phải có độ dài từ 5 đến 200 ký tự'),
+        body('startDate')
+            .notEmpty()
+            .withMessage('Ngày bắt đầu là bắt buộc')
+            .bail()
+            .isDate()
+            .withMessage('Ngày bắt đầu không hợp lệ')
+            .bail(),
+        body('roles')
+            .isArray({ min: 1 })
+            .withMessage('Danh sách vai trò là bắt buộc và phải là mảng')
+            .bail()
+            .custom((roles) => {
+                console.log(roles);
+    
+                const invalidRoles = roles.filter((role) => !ALLOWED_ROLES.includes(role.roleName));
+                if (invalidRoles.length > 0) {
+                    throw new Error(`Các vai trò không hợp lệ: ${invalidRoles.join(', ')}`);
+                }
+                return true;
+            }),
+        body('warehouseID').notEmpty().withMessage('Warehouse ID là bắt buộc').bail(),
+]
+
 module.exports = {
     checkEmail,
+    checkUpdateEmployee
 };
