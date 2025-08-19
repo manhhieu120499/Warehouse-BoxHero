6501b6b97e0d1334ffb6610c43dafc79c528248b
Bao <hbao27121@gmail.com>
2025-08-19 07:06:18 +0700

    commit feature change password

--- Backend/src/routes/AccountRouter.js
+++ Backend/src/routes/AccountRouter.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const AccountController = require('../controllers/AccountController');
-const { checkEmailExists, checkSignUpValidate, checkSignInValidate } = require('../validates/account.validation');
+const {
+    checkEmailExists,
+    checkSignUpValidate,
+    checkSignInValidate,
+    changePasswordValidate,
+} = require('../validates/account.validation');
 const validate = require('../validates/validate');
-const { authUserIsManager } = require('../middleware/AuthMiddleware');
+const { authUserIsManager, authUser } = require('../middleware/AuthMiddleware');
 
 router.get('/check-email-exists', checkEmailExists, validate, AccountController.checkEmailExists);
 router.post('/sign-up', authUserIsManager, checkSignUpValidate, validate, AccountController.signUp);
 router.post('/sign-in', checkSignInValidate, validate, AccountController.signIn);
+router.post('/change-password', authUser, changePasswordValidate, validate, AccountController.changePassword);
 
 module.exports = router;
