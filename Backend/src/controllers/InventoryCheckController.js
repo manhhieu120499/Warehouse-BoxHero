d7073467c29baf98739f7222f26dc1b021d30b44
Bao <hbao27121@gmail.com>
2025-10-03 12:50:13 +0700

    commit feature inventory check

--- Backend/src/controllers/InventoryCheckController.js
+++ Backend/src/controllers/InventoryCheckController.js
@@ -9,6 +9,24 @@ class InventoryCheckController {
             return res.status(err.statusHttp).json([err.message]);
         }
     }
+
+    async createInventoryCheck(req, res) {
+        try {
+            const { statusHttp, ...response } = await InventoryCheckService.createInventoryCheck(req.body);
+            return res.status(statusHttp).json(response);
+        } catch (err) {
+            return res.status(err.statusHttp).json([err.message]);
+        }
+    }
+
+    async filterInventoryCheck(req, res) {
+        try {
+            const { statusHttp, ...response } = await InventoryCheckService.filterInventoryCheck(req.query);
+            return res.status(statusHttp).json(response);
+        } catch (err) {
+            return res.status(err.statusHttp).json([err.message]);
+        }
+    }
 }
 
 module.exports = new InventoryCheckController();
