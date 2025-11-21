fe4fea91b3af6f5ecea37841cf5a2767ad02d1a9
Hiếu <vomanhhieu62@gmail.com>
2025-11-21 15:01:27 +0700

    feat: set up initial mobile application with Redux and navigation

--- Box-Hero-Mobile/App.js
+++ Box-Hero-Mobile/App.js
@@ -2,13 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Navigation from './src/navigation/Navigation';
 import Toast from 'react-native-toast-message';
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Navigation />
-            <Toast />
-        </View>
+        <Provider store={store}>
+            <View style={styles.container}>
+                <Navigation />
+                <Toast />
+            </View>
+        </Provider>
     );
 }
 
